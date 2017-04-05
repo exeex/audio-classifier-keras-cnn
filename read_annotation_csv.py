69b20f0bd3105644be614600ee50abe426e1f4cd
cswu <xray0h@gmail.com>
2017-04-05 19:14:09 +0800

    before going home

--- read_annotation_csv.py
+++ read_annotation_csv.py
@@ -6,10 +6,7 @@ import csv
 #
 
 filename = "annotations_final.csv"
-#TODO : dssign a function to sum up the tag count to find out popular tags
-
-
-
+#DONE : dssign a function to sum up the tag count to find out popular tags
 
 
 def get_table():
@@ -22,27 +19,15 @@ def get_table():
     f.close()
     return table
 
-def get_dict():
-    f = open(filename, 'r')
-    dict = csv.DictReader(f, delimiter='\t')
-    return dict
-
 
 def get_tags(table):
     return table[0]
 
-# def tag_count(table, tag):
-#     count = 0
-#
-#     for element in table:
-#         #print(row[tag])
-#         if element[tag] == '1' :
-#             count+=1
-#     print(count)
-#     return count
+def transpose_table(table):
+    return list(map(list, zip(*table)))
 
 def tag_count(table, tags):
-    tt = list(map(list, zip(*table)))
+    tt = transpose_table(table)
     counts=[]
     for x in range(1,len(tt)):
         counts.append([tags[x],tt[x].count('1')])
@@ -56,6 +41,43 @@ def get_hot_tags():
     return sorted(counts,key =lambda x:x[1] , reverse = True)
 
 
+
 #TODO : design a function to pick some tag
+
+def get_dict():
+    f = open(filename, 'r')
+    dict = csv.DictReader(f, delimiter='\t')
+    return dict
+
+def write_csv_subset(filename= "annotations_subset.csv" , tags=[]):
+    d = get_dict()
+    with open(filename, 'w') as csvfile:
+        fieldnames = []
+        fieldnames.append("clip_id")
+        for tag in tags:
+            fieldnames.append(tag)
+        fieldnames.append("mp3_path")
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+
+
+
+        writer.writeheader()
+        writer.writerow({'clip_id': '0'})
+        writer.writerow({'clip_id': '1'})
+        writer.writerow({'clip_id': '2'})
+
+
+
+table = get_table()
+tt = transpose_table(table)
+tags = get_tags(table)
+
+for row in tt[0] :
+    if row[0] not in tags:
+
+
+
 
 #TODO : out put subset of annotation data with tags we picked
