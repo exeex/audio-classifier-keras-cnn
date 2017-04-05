21c5552671fc76ba76064b1064e38c2a5b6457a2
cswu <xray0h@gmail.com>
2017-04-05 17:53:04 +0800

    hot tag counts

--- read_annotation_csv.py
+++ read_annotation_csv.py
@@ -6,25 +6,54 @@ import csv
 #
 
 filename = "annotations_final.csv"
-f = open(filename, 'r')
-
 #TODO : dssign a function to sum up the tag count to find out popular tags
 
-def get_tags(f):
-    for idx , row in enumerate(csv.reader(f,delimiter='\t')):
-        if idx ==0 :
-            print(row)
-
-def tag_count(file = f, tag = "no voice"):
-    count = 0
-    for row in csv.DictReader(file,delimiter='\t'):
-        #print(row[tag])
-        if row[tag] == '1' :
-            count+=1
-
-    print(count)
 
 
+
+
+def get_table():
+    f = open(filename, 'r')
+    csv_cursor = csv.reader(f, delimiter='\t')
+
+    table = []
+    for row in csv_cursor:
+        table.append(row)
+    f.close()
+    return table
+
+def get_dict():
+    f = open(filename, 'r')
+    dict = csv.DictReader(f, delimiter='\t')
+    return dict
+
+
+def get_tags(table):
+    return table[0]
+
+# def tag_count(table, tag):
+#     count = 0
+#
+#     for element in table:
+#         #print(row[tag])
+#         if element[tag] == '1' :
+#             count+=1
+#     print(count)
+#     return count
+
+def tag_count(table, tags):
+    tt = list(map(list, zip(*table)))
+    counts=[]
+    for x in range(1,len(tt)):
+        counts.append([tags[x],tt[x].count('1')])
+    return counts
+
+
+def get_hot_tags():
+    t = get_table()
+    tags = get_tags(t)
+    counts = tag_count(t,tags)
+    return sorted(counts,key =lambda x:x[1] , reverse = True)
 
 
 #TODO : design a function to pick some tag
